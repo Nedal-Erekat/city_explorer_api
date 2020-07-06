119e9bfca383cb3348cbfac4a9139ce1583a879e
Nedal_Erekat <nedalerekat@gmail.com>
2020-07-06 14:31:56 +0300

    startServer

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 'use strict';
 //dependensies
-const express=Requier('express');
-const cons=requier('cors');
-requier('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 3030;
+const app = express();
+app.use(cors());//anyone can touch my server
+app.listen(PORT, () => {
+    console.log(`port ${PORT}`);
+});
