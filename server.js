ad03d040edec5f254c71edea1dcb4ae8dd15468c
Nedal_Erekat <nedalerekat@gmail.com>
2020-07-06 17:10:52 +0300

    creatWeather

--- server.js
+++ server.js
@@ -32,6 +32,38 @@ function Location(city,getData) {
     this.latitude=getData[0].lat;
     this.longitude=getData[0].lon;
 }
+function Weather(weatherData) {
+    // [
+    //     {
+    //       "forecast": "Partly cloudy until afternoon.",
+    //       "time": "Mon Jan 01 2001"
+    //     },
+    //     {
+    //       "forecast": "Mostly cloudy in the morning.",
+    //       "time": "Tue Jan 02 2001"
+    //     },
+    //     ...
+    //   ]
+    this.forecast=weatherData.weather.description;
+    this.time=weatherData.datetime;
+}
+
+
+app.get('/weather',(req,res)=>{
+    // const city = req.query.data;
+    const getWeatherData=require('./data/weather.json');
+    // console.log(getWeatherData.data[0].weather.description);
+    let data=[];
+    getWeatherData.data.forEach(element => {
+        
+        const creatWeather=new Weather(element);
+        data.push(creatWeather);
+    });
+    res.send(data);
+
+})
+
+
 app.get('*',(req,res)=>{
     res.send('not fond');
 });
