267ff171029591432e31fdd5aa60eb01507bdf2d
Nedal_Erekat <nedalerekat@gmail.com>
2020-07-07 17:13:55 +0300

    trailsDeployed

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get('/', (req, res) => {
 // http://localhost:3000/location?data=amman
 app.get('/location', hitLocation);
 app.get('/weather',hitWeather);
+app.get('/trails',hitTrails);
 
 // Route Handlers
 
@@ -56,14 +57,14 @@ function Location(city, getData) {
 }
 
 function hitWeather(req, res) {
-        getWeather(city)
+        getWeather()
     .then(data=>{
         
         res.status(200).json(data);
     });     
 };
-function getWeather(city) {
-    console.log(Location.all);
+function getWeather() {
+    // console.log(Location.all);
     
     let key=process.env.WEATHER_API_KEY;
     let url=`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${Location.all[0].latitude}&lon=${Location.all[0].longitude}&key=${key}`;
@@ -82,9 +83,56 @@ function Weather(weatherData) {
 
     this.forecast = weatherData.weather.description;
     this.time = weatherData.datetime;
+};
+
+
+function hitTrails(req,res) {
+    getTrials()
+    .then(data=>{
+        res.status(200).json(data);
+    });
 }
 
+function getTrials() {
+    let key=process.env.TRAIL_API_KEY;
+    let url=`https://www.hikingproject.com/data/get-trails?lat=${Location.all[0].latitude}&lon=${Location.all[0].longitude}&key=${key}`;
+    return superagent.get(url)
+    .then(data=>{
+        console.log('Here it is the data:>>>>>>>>>'+ data.body.trails);
+        
+        let trialData=data.body.trails.map((ele,i)=>{
+            const creatTrail= new Trial(ele)
+            return creatTrail;
+        });
+        return trialData;
 
+    });
+}
+
+// {
+//     "name": "Mt. Si",
+//     "location": "Tanner, Washington",
+//     "length": "6.6",
+//     "stars": "4.4",
+//     "star_votes": "72",
+//     "summary": "A steep, well-maintained trail takes you atop Mt. Si with outrageous views of Puget Sound.",
+//     "trail_url": "https://www.hikingproject.com/trail/7001016/mt-si",
+//     "conditions": "Dry",
+//     "condition_date": "2018-07-22",
+//     "condition_time": "0:17:22 "
+//   },
+function Trial(params) {
+    this.name=params.name;
+    this.location=params.location;
+    this.length=params.length;
+    this.stars=params.stars;
+    this.star_votes=params.star_votes;
+    this.summary=params.summary;
+    this.trail_url=params.trail_url;
+    this.condition=params.condition;
+    this.condition_date=params.condition_date;
+    this.condition_time=params.condition_time;
+}
 
 
 app.get('*', (req, res) => {
